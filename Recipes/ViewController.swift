97ce7afeee1445d3a47d40ad9d1dd0280db8e39f
Scott Cox <jscox5@gmail.com>
2022-05-16 09:20:40 -0600

    initial commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Scott Cox on 5/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
