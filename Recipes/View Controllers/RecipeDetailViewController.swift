64ae356e5150974b9585e1fc284d05c210e196a1
Scott Cox <jscox5@gmail.com>
2022-05-16 10:01:37 -0600

    basic storyboards created

---
+++ Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Scott Cox on 5/16/22.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var recipeTitleTextField: UITextField!
+    @IBOutlet weak var calorieCountTextField: UITextField!
+    @IBOutlet weak var cookTimeTextField: UITextField!
+    @IBOutlet weak var recipeDescriptionTextView: UITextView!
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+    
+    
+    // MARK: - Actions
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+  
+    // MARK: - Navigation
+
+   
+} // End of Class
